3f9bee72cd0bdb56676326349721a3c25d77d64d
Antoni Aloy Torrens <aaloytorrens@gmail.com>
2026-08-11 14:55:30 +0200

    Compress img after finishing

--- scripts/finalize_image.sh
+++ scripts/finalize_image.sh
@@ -4,6 +4,8 @@ set -eu -o pipefail
 # Rename kiwi's raw disk image(s) to .img, optionally xz-compressing them.
 # Usage: finalize_image.sh <output-dir> <compress:0|1>
 
+echo "Executing $0 ..."
+
 outdir=$1
 compress=$2
 
@@ -12,6 +14,9 @@ for raw in "$outdir"/*.raw; do
 	img="${raw%.raw}.img"
 	mv -f "$raw" "$img"
 	if [ "$compress" = "1" ]; then
-		xz -T0 -k -f "$img"
+		echo "Compressing image $img to .xz ..."
+		xz -v -T0 -k -f "$img"
 	fi
 done
+
+echo "Finished executing $0 ."
